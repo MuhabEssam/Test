60469e7b1625a2da5d598ee9e35e97b98e4b1afd
MHB <muhabahmed43@yahoo.com>
2022-10-19 09:57:08 +0200

    create project folder

--- secondform2.py
+++ secondform2.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import  *
 from PyQt5.QtCore import *
 from PyQt5.uic import loadUiType
 from os import path
-import sys
+import sys,os
 from  Last_window import mainapp
 Form_class2,_=loadUiType(path.join(path.dirname(__file__),"secondform.ui"))
 class Second(QWidget,Form_class2):
@@ -11,11 +11,17 @@ class Second(QWidget,Form_class2):
         super(Second,self).__init__(parent)
         QWidget.__init__(self)
         self.setupUi(self)
+        self.objPath=""
+        self.objName=""
         self.handle_button()
     def Show_files(self):
-        win=QFileDialog.getOpenFileName(self,'Save At','C:\\')
-        self.lineEdit_2.setText(win[0])
+        self.objPath=QFileDialog.getExistingDirectory(self,'Save At','C:\\')
+        self.objName=self.lineEdit.text()
+        self.lineEdit_2.setText(self.objPath)
+    def Create_folder(self):
+        os.mkdir(os.path.join(self.objPath,self.objName))
     def openWin2(self):
+        self.Create_folder()
         self.window2=mainapp()
         self.window2.show()
     def handle_button(self):
